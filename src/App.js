4ea51980c818540918ba4c1b3bec699e996a8f9f
Alex Qiang <alex@Alex-Laptop.local>
2025-12-29 22:47:55 +1100

    Update navbar

--- src/App.js
+++ src/App.js
@@ -35,14 +35,13 @@ const App = (params) => {
     <BrowserRouter>
       <Container className="p-0" fluid={true}>
         <Navbar className="border-bottom" bg="transparent" expand="lg">
-          <Navbar.Brand>Alex Qiang</Navbar.Brand>
+          <Navbar.Brand>
+            <Link className="navbar-brand" to="/">Alex Qiang</Link>
+          </Navbar.Brand>
 
           <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
           <Navbar.Collapse id="navbar-toggle">
             <Nav className="ml-auto">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
               <Link className="nav-link" to="/about">
                 About
               </Link>
